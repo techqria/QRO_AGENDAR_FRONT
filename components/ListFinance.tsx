80e49a856b1258ad87222bd6eec12889dfe04745
aenewsss <aenamartinelli@gmail.com>
2023-05-04 23:32:53 -0300

    [FUNC] cards page check

--- components/ListFinance.tsx
+++ components/ListFinance.tsx
@@ -15,7 +15,7 @@ const ListFinance = () => {
         <div className="w-100 d-flex flex-column gap-3 mt-5">
             {
                 employees.map(el => (
-                    <div className="d-flex justify-content-around bg-white pt-3 pb-3">
+                    <div className="d-flex justify-content-between bg-white p-3">
                         <div className="d-flex flex-column">
                             <h5 className="text-black fw-bold">{el.firstName}</h5>
                             <span className="text-black">{el.lastName}</span>
@@ -31,9 +31,9 @@ const ListFinance = () => {
                             <span className="text-black">Valor total</span>
                         </div>
 
-                        <div className="d-flex align-items-center">
+                        <button data-bs-toggle="modal" data-bs-target="#listFinancesModal" className="d-flex align-items-center p-0 m-0 btn btn-transparent">
                             <img role="button" src="https://raw.githubusercontent.com/gist/Aenewsss/fbe5368d795b9aececfdef9fa3c1edc9/raw/ab991ac12aa87c5ad47f3a8a2998415561c6b74e/modal-expand-qro-agendar.svg" alt="" />
-                        </div>
+                        </button>
                     </div>
                 ))
             }
--- pages/_deprecated/financas.tsx
+++ pages/_deprecated/financas.tsx
@@ -1,13 +1,15 @@
 import CardsFinance from "../../components/CardsFinance";
 import ListFinance from "../../components/ListFinance";
+import ModalListFinances from "../../components/Modals/ModalListFinances";
 
 const Finance = () => {
 
     return (
-        <section className='container bg-white-sec h-100vh d-flex flex-column justify-content-center align-items-center'>
-            <h2 className="w-100 text-start text-black">Finanças</h2>
+        <section className='container bg-white-sec h-100 d-flex flex-column justify-content-center align-items-center pt-md-0 pt-4'>
+            <h2 className="w-100 text-start text-black mt-5 pt-5">Finanças</h2>
             <CardsFinance />
             <ListFinance />
+            <ModalListFinances />
         </section>
     );
 }
