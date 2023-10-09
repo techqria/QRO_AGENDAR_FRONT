c456014eca244526764154b97e5eb578f5aeba50
aenewsss <aenamartinelli@gmail.com>
2023-10-09 09:31:14 -0300

    new env

--- graphql/graphql-client.ts
+++ graphql/graphql-client.ts
@@ -4,7 +4,7 @@ import { HttpLink } from "apollo-link-http";
 
 export const apolloClient = new ApolloClient({
     link: new HttpLink({
-        uri: 'https://qro-agendar-back.vercel.app',
+        uri: 'https://qro-agendar-back.vercel.app/graphql',
         headers: {
             "Authorization": "Bearer " + (typeof window !== 'undefined' ? window.localStorage.getItem("token") : null)
         }
