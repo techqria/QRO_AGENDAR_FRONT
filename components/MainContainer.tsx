aebae1be57af63edb1073d3b893f0dea9312f1a8
aenewsss <aenamartinelli@gmail.com>
2023-10-08 23:15:09 -0300

    ok

--- components/MainContainer.tsx
+++ components/MainContainer.tsx
@@ -10,8 +10,8 @@ const MainContainer = ({ children }) => {
                 <meta name="description" content="QRO AGENDAR. Agendar ficou muito mais fácil." />
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" />
                 <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+                <script crossOrigin="anonymous" src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" />
             </Head>
-            <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" />
 
             <main>
                 <Toast />
