bbebadc1cd9a157b738f0db8ee7474100fea5d66
aenewsss <aenamartinelli@gmail.com>
2023-10-09 11:19:14 -0300

    remove integrity bootstrap

--- components/MainContainer.tsx
+++ components/MainContainer.tsx
@@ -10,7 +10,7 @@ const MainContainer = ({ children }) => {
                 <meta name="description" content="QRO AGENDAR. Agendar ficou muito mais fácil." />
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" />
                 <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" async />
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"  async />
             </Head>
 
             <main>
